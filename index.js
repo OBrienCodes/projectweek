1c0c70bf600126d3b29f5c9da880d7d7313635bb
github username <github email address>
2021-06-22 17:36:29 -0400

    characters added

--- index.js
+++ index.js
@@ -2,4 +2,25 @@
 
 fetch(`https://thronesapi.com/api/v2/Characters`)
 .then(res => res.json())
-.then(data => console.log(data))
+.then(characters => characters.forEach(renderCharacters))
+
+const renderCharacters = (character) => {
+    
+    const characterCollection = document.querySelector("#character-collection")
+
+    const characterCard = document.createElement('div')
+    characterCard.className = 'card'
+
+    const characterName = document.createElement('h2')
+    characterName.innerText = character.fullName
+    
+    const characterImage = document.createElement('img')
+    characterImage.src = character.imageUrl
+
+    
+
+
+    characterCard.append(characterName, characterImage)
+    characterCollection.append(characterCard)
+
+}
